959112ea06af9bc2e6805bd1a3fba3f92a993108
MythEclipse <asepharyanalm001@gmail.com>
2025-10-20 13:37:25 +0700

    Add .gitignore to exclude JAR files and implement main function with updated user details

---
+++ Modul1/1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var nama: String = "Dede"
+    var asal = "Kuningan"
+    var usia = 25
+
+    nama = "Dede Husen"
+    asal = "Kuningan Jawa Barat"
+    usia = 30
+
+    println(nama)
+    println(asal)
+    println(usia)
+}
