0646374fac619ac023a2f6f81b74c9fde07daa9d
MythEclipse <asepharyanalm001@gmail.com>
2025-11-05 15:43:02 +0700

    Add activities and layouts for calculating area of square and triangle

--- Modul3Kalkulator/app/src/main/java/com/mytheclipse/modul3kalkulator/PersegiActivity.kt
+++ Modul3Kalkulator/app/src/main/java/com/mytheclipse/modul3kalkulator/PersegiActivity.kt
@@ -1,12 +1,35 @@
 package com.mytheclipse.modul3kalkulator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class PersegiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_persegi)
+
+        val btn_hitung = findViewById<Button>(R.id.btn_hitung)
+        val sisi = findViewById<EditText>(R.id.edt_sisi)
+        val hasil = findViewById<TextView>(R.id.hasil)
+
+        btn_hitung.setOnClickListener {
+            val sisiValue = sisi.text.toString().toDouble()
+            val luas = sisiValue * sisiValue
+            hasil.text = luas.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
 
--- Modul3Kalkulator/app/src/main/java/com/mytheclipse/modul3kalkulator/SegitigaActivity.kt
+++ Modul3Kalkulator/app/src/main/java/com/mytheclipse/modul3kalkulator/SegitigaActivity.kt
@@ -1,12 +1,37 @@
 package com.mytheclipse.modul3kalkulator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class SegitigaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_segitiga)
+
+        val btn_hitung = findViewById<Button>(R.id.btn_hitung)
+        val alas = findViewById<EditText>(R.id.edt_alas)
+        val tinggi = findViewById<EditText>(R.id.edt_tinggi)
+        val hasil = findViewById<TextView>(R.id.hasil)
+
+        btn_hitung.setOnClickListener {
+            val alasValue = alas.text.toString().toDouble()
+            val tinggiValue = tinggi.text.toString().toDouble()
+            val luas = 0.5 * alasValue * tinggiValue
+            hasil.text = luas.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
 
